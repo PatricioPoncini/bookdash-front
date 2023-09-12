1728aead6cebdf8c2809d9544cf70483e2e6790d
PatricioPoncini <patricioponcini10@gmail.com>
2023-09-12 09:13:59 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/">BookDash</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/about">About</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+.navbar {
+  background: rgb(0, 0, 255);
+  background: linear-gradient(80deg, rgba(0, 0, 255, 1) 0%, rgba(0, 166, 255, 1) 87%);
+}
+
+.navbar a {
+  color: #F6F6F6;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <div class="row">
+      <div class="col-12">
+        <h1 class="text-center">WELCOME TO BOOKDASH</h1>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400;500&display=swap');
+
+h1 {
+  font-size: 50px;
+  font-weight: 400;
+  font-family: Montserrat, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  margin-top: 200px;
+}
+</style>
